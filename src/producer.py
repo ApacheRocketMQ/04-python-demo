7c68dfa80c166f4bc6c117002cf574bd71e68067
tigerlee <tigerlee@apache.org>
2022-03-23 14:13:14 +0800

    update

---
+++ src/producer.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from rocketmq.client import Producer, Message, TransactionMQProducer, TransactionStatus
+
+import time
+
+topic = 'tigerweili-topic-01'
+producer_group_name = 'tigerweili-producer-group-01'
+name_srv = '127.0.0.1:9876'
+
+
+def create_message():
+    msg = Message(topic)
+    msg.set_keys('XXX')
+    msg.set_tags('XXX')
+    msg.set_property('property', 'test')
+    msg.set_body('message body')
+    return msg
+
+
+def send_message_sync(count):
+    producer = Producer(producer_group_name)
+    producer.set_name_server_address(name_srv)
+    producer.start()
+    for n in range(count):
+        msg = create_message()
+        ret = producer.send_sync(msg)
+        print ('[sending message]: ' + str(ret.status) + ' msgId: ' + ret.msg_id)
+    print ('send sync message done')
+    producer.shutdown()
+
+
+if __name__ == '__main__':
+    send_message_sync(10)
+
+
